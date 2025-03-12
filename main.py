0a1a085072d3a94d5dfd960ac0583efff0ef9d63
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-03-11 20:22:52 -0500

    add new line class with point classes to draw in window

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from window import window
+from window import Window, Line, Point
 
 def main():
-    root_window = window(800, 600)
+    root_window = Window(800, 600)
+    left_top = Point(0, 0)
+    right_bottom = Point(800, 600)
+    cross_line = Line(left_top, right_bottom)
+    root_window.draw_line(cross_line, 'black')
     root_window.wait_for_close()
 
 
