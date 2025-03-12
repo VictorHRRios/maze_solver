bbae2638faa4e671baca3761a25f201560a18d1c
VictorHRRios <96360085+VictorHRRios@users.noreply.github.com>
2025-03-11 20:59:54 -0500

    add line to chow path for the future

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
-from window import Window, Line, Point
+from window import Window, Cell
 
 def main():
     root_window = Window(800, 600)
-    left_top = Point(0, 0)
-    right_bottom = Point(800, 600)
-    cross_line = Line(left_top, right_bottom)
-    root_window.draw_line(cross_line, 'black')
+    first_cell = Cell(5, 5, 55, 55, root_window)
+    first_cell.has_top_wall = True
+    first_cell.has_bottom_wall = True
+    first_cell.has_left_wall = True
+    first_cell.draw('black')
+
+    second_cell = Cell(55, 5, 105, 55, root_window)
+    second_cell.has_top_wall = True
+    second_cell.has_right_wall = True
+    second_cell.draw('black')
+
+    first_cell.draw_move(second_cell)
+
+    third_cell = Cell(55, 55, 105, 105, root_window)
+    third_cell.has_bottom_wall = True
+    third_cell.has_left_wall = True
+    third_cell.has_right_wall = True
+    third_cell.draw('black')
+
+    second_cell.draw_move(third_cell)
+
     root_window.wait_for_close()
 
 
--- window.py
+++ window.py
@@ -37,3 +37,48 @@ class Line():
 
     def draw(self, canvas, fill_color):
         canvas.create_line(self.point_1.x, self.point_1.y, self.point_2.x, self.point_2.y, fill = fill_color, width = 2)
+
+class Cell():
+    def __init__(self, x1, y1, x2, y2, window):
+        self.has_left_wall = False
+        self.has_right_wall = False
+        self.has_top_wall = False
+        self.has_bottom_wall = False
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2
+        self._win = window
+
+    def draw(self, fill_color):
+        point_tl = Point(self._x1, self._y1)
+        point_tr = Point(self._x2, self._y1)
+        point_bl = Point(self._x1, self._y2)
+        point_br = Point(self._x2, self._y2)
+        if self.has_left_wall:
+            left_line = Line(point_tl, point_bl)
+            self._win.draw_line(left_line, fill_color)
+        if self.has_right_wall:
+            right_line = Line(point_tr, point_br)
+            self._win.draw_line(right_line, fill_color)
+        if self.has_top_wall:
+            top_line = Line(point_tl, point_tr)
+            self._win.draw_line(top_line, fill_color)
+        if self.has_bottom_wall:
+            bottom_line = Line(point_bl, point_br)
+            self._win.draw_line(bottom_line, fill_color)
+
+    def draw_move(self, to_cell, undo=False):
+        center_from_x = (self._x2 - self._x1) // 2 + self._x1
+        center_from_y = (self._y2 - self._y1) // 2 + self._y1
+        center_point_from = Point(center_from_x, center_from_y)
+        print(center_from_x, center_from_y)
+        center_to_x = (to_cell._x2 - to_cell._x1) // 2 + to_cell._x1
+        center_to_y = (to_cell._y2 - to_cell._y1) // 2 + to_cell._y1
+        print(center_to_x, center_to_y)
+        center_point_to = Point(center_to_x, center_to_y)
+        from_to_line = Line(center_point_from, center_point_to)
+        if undo:
+            self._win.draw_line(from_to_line, 'gray')
+        else:
+            self._win.draw_line(from_to_line, 'red')
